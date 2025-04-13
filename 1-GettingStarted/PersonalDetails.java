d024c55c42cb03abe0fee10c0ce76d306ed81233
Sai Joshi <sdjoshiaz8@gmail.com>
2025-04-13 16:25:34 +0545

    week 1 codes

---
+++ 1-GettingStarted/PersonalDetails.java
@@ -0,0 +1,20 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+    
+        String studentName = "Sai Joshi";
+        String address = "Chabahil";
+        String phoneNumber = "984107595";
+        int age = 19;
+        String HouseID = "HouseId1919";
+        String studentGrade = "A";
+
+    
+        System.out.println("Personal Details:");
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Age: " + age);
+        System.out.println("House ID: " + HouseID);
+        System.out.println("Student Grade: " + studentGrade);
+    }
+}
